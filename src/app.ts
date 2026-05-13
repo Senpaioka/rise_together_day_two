2bf954d48d468236a92249fa340d550bf0433425
senpaioka@windows <abdulmannan7589@gmail.com>
2026-05-13 15:15:47 +0600

    basic setup

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import express, { type Application, type NextFunction, type Request, type Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import router from './routes/routers';
+
+const app: Application = express();
+
+// global middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// server health check
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+        success: true,
+        message: "Server is running."
+    });
+});
+
+// Not found route
+app.use( (req: Request, res: Response) => {
+    res.status(404).json({
+        success: false,
+        message: "Route not found!"
+    });
+});
+
+// global error handler
+app.use( (err: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+
+    res.status(500).json({
+        success: false,
+        message: "Something went wrong!"
+    });
+});
+
+// routes
+app.use('/api/v1', router);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,59 @@
+import http from "http";
+import app from "./app";
+import env from "./config/env";
+
+const server = http.createServer(app);
+
+async function bootstrap(): Promise<void> {
+  try {
+    server.listen(env.PORT, () => {console.log(`🚀 Server running successfully at http://localhost:${env.PORT} 📡 Listening on port ${env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Server startup failed:", error);
+    process.exit(1);
+  }
+}
+
+bootstrap();
+
+
+// Unhandled Rejection
+process.on(
+  "unhandledRejection",
+  (reason: unknown) => {
+    console.error(
+      "UNHANDLED REJECTION:",
+      reason
+    );
+
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+);
+
+
+// Uncaught Exception
+process.on(
+  "uncaughtException",
+  (error: Error) => {
+    console.error(
+      "UNCAUGHT EXCEPTION:",
+      error
+    );
+
+    process.exit(1);
+  }
+);
+
+
+// SIGTERM
+process.on("SIGTERM", () => {
+  console.log(
+    "👋 SIGTERM received. Shutting down..."
+  );
+
+  server.close(() => {
+    process.exit(0);
+  });
+});
